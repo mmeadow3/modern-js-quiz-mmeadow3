b771367d7e36916200fbf278486fbaa897b80066
Mike Meadows <mmeadow33@gmail.com>
2016-07-14 12:46:18 -0500

    MVP completed, all functionality is working

--- javascripts/DOMhandler.js
+++ javascripts/DOMhandler.js
@@ -7,19 +7,21 @@ let Name = require('./names');
 
 let selected = {Robot, Type, Name};
 var players = [];
+var users = [];
 
 
-
-$(document).ready(function() {
+// $(document).ready(function() {
 /////////////////gets value from text input box//////////
 $('#p1Select').click(function(){
 	var p1 = $('#p1Input').val();
 	$('#p1Output').prepend(p1 + " fighting in the ");
+	users.push(p1);
 });
 
 $('#p2Select').click(function(){
 	var p2 = $('#p2Input').val();
 	$('#p2Output').prepend(p2 + " fighting in the ");
+	users.push(p2);
 });
 
 /////////////////select class of robots//////////////////////
@@ -51,12 +53,12 @@ if (selected) {
 	console.log(robot1);
 	players.push(robot1);
 	$('#p1Output').append(robot1.name + " variant of " + robot1.type);
-	$('#player1Select > li').find("button[type='button']").prop('disabled',true)
+	$('#player1Select > li').find("button[type='button']").prop('disabled',true);
 	$('#create1').one('click', function(){
 	let player1 = selected;
-	$('#battleArena').append(`<div id="introText1"> Here is Player 1's ${robot1.name}</div>`)
-    })
-  };
+	$('#battleArena').append(`<div id="introText1"> Here is Player 1's ${robot1.name}<br>Your health is ${robot1.health}</div>`);
+    });
+  }
 });
 
 
@@ -86,25 +88,43 @@ $('#player2Select > li').click(function(){
 if (selected) {
 	let robot2 = selected;
 	console.log(robot2);
-	players.push(robot2)
+	players.push(robot2);
 	$('#p2Output').append(robot2.name + " variant of " + robot2.type);
-	$('#player2Select > li').find("button[type='button']").prop('disabled',true)
+	$('#player2Select > li').find("button[type='button']").prop('disabled',true);
 	$('#create2').one('click',function(){
 	let player2 = selected;
-	$('#battleArena').append(`<div id="introText2"> Here is Player 2's ${robot2.name}</div>`)
+	$('#battleArena').append(`<div id="introText2"> Here is Player 2's ${robot2.name}<br>Your health is ${robot2.health}</div>`);
 	console.log(players);
     });
-  };
+  }
 });
 
 
 
-//////////////////attack button/////////////////////
+// //////////////////attack button/////////////////////
 $('#attackBtn').click(function(){
-	console.log(players)
+	let battleLog = $('#battleArena');
+	  if (players[0].health > 0) {
+	  	battleLog.append(`<div>You did ${players[0].damage} damage. Enemy health is now ` + (players[1].health-players[0].damage) + `</div>`);
+	 } 
+	 players[1].health = (players[1].health-players[0].damage);
+	 players[0].health = (players[0].health-players[1].damage);
+	  // if(players[1].health >= 0){  //////////,maybe remvoe this////////
+		setTimeout(function(){
+			if(players[1].health >= 0 && players[0].health >= 0) {
+			battleLog.append(`<div> Enemy attacks with ${players[1].damage} damage. Your health is now ` + (players[0].health) + `</div>`);
+			} else if (players[1].health <= 0) {
+	    		battleLog.append(`<h4>You have deafeated the enemy ${players[1].name} with the ${players[0].name} ${players[0].type} using the ${players[1].standardWeapon} weapon</h4>`);
+	    		$(this).prop("disabled",true);
+	    	} else if (players[0].health <= 0  && players[0].health) {
+		 	battleLog.append(`<div> Enemy attacks with ${players[1].damage} damage. Your health is now ` + (players[0].health) + `</div>`);
+			battleLog.append(`<h4>You have been deafeated by the enemy ${players[1].name} ${players[0].type} using the ${players[1].standardWeapon} weapon</h4>`);
+		}
+	   
+}, 2000);
+		
 });
 
+// players[0].health = (players[0].health-players[1].damage);
 
-
-});
 module.exports = players;
